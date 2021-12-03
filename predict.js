7761963a0f413cf200e15a8a61da8310e0130738
Maurycy Z <10maurycy10@gmail.com>
2021-12-03 09:05:36 -0800

    work

--- predict.js
+++ predict.js
@@ -6,6 +6,10 @@ var rl = readline.createInterface({
   terminal: true
 });
 
+let SubjectChangers = [""]
+
+let excu = ["What?"]
+
 function rank(p,np,len,inlen) {
 	let rank = p-(Math.abs(len-10)/30)
 	//console.log(p,np,len,inlen,rank)
@@ -24,6 +28,7 @@ function find_best_match(input_string,ref) {
 
   let max = -100
   let idx = -1
+  let sel_p = -1
 
   for (senid in ref) {
     let sen = ref[senid]
@@ -44,11 +49,12 @@ function find_best_match(input_string,ref) {
     if (rank(p,np,swords.length,input.length) > max) {
        max = rank(p,np,swords.length,input.length)
        idx = senid
+       sel_p = p
        //console.log(ref[senid])
     }
 
   }
-  return idx;
+  return [idx,max,sel_p];
 }
 
 fs.readFile('big.txt', 'utf8' , (err, text) => {
@@ -58,16 +64,37 @@ fs.readFile('big.txt', 'utf8' , (err, text) => {
     return
   }
 
-  let lines = text.split('\n').filter((x) => x.length > 1);
+  let lines = text.split('\n').filter((x) => x.length > 1).filter((x) => x[0] != ">");
 
   rl.on("line", (x) => {
-    let bestidx = find_best_match(x ,lines)
+    let match = find_best_match(x ,lines)
+
+    let bestidx = match[0];
+
+    let rank = match[1];
 
     let resp = lines[parseInt(bestidx,10)+1]
 
-    console.log(`<< ${lines[bestidx]} : ${resp}`)
+    if (match[2]>0)
+      console.log(`<< ${resp}`)
+    else {
+       let e = Math.floor(Math.random() * excu.length)
+       console.log(`<< ${excu[e]}`)
+    }
 
     process.stdout.write("> ");
 
   })
+})
+
+fs.readFile('excuses', 'utf8' , (err, text) => {
+
+  if (err) {
+    console.error(err)
+    return
+  }
+
+  let lines = text.split('\n').filter((x) => x.length > 1);
+
+  excu = lines
 })
